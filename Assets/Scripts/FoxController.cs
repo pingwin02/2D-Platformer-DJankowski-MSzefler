8f6f5c78fdcbe73ee5fd48a55347848dc80b8eb1
rysiekpol <ma.szefler@gmail.com>
2022-11-24 23:14:26 +0100

    death system, simple animation

--- Assets/Scripts/FoxController.cs
+++ Assets/Scripts/FoxController.cs
@@ -14,21 +14,39 @@ public class FoxController : MonoBehaviour
 
     const float rayLength = 1.00f;
 
+    private Collider2D _collider;
+
+    private bool active = true;
+
+    private Vector3 startPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        _collider = GetComponent<Collider2D>();
+        SetStartingPosition();
 
     }
     // Update is called once per frame
     void Update()
     {
+        if (!active)
+        {
+            return;
+        }
+
+
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
             transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
             transform.Translate(-moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
         if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
             Jump();
+
+        if (transform.position.y < -5)
+        {
+            Die();
+        }
 
         /*
         Debug.DrawRay(transform.position, rayLength * Vector3.down, Color.white, 1, false);
@@ -56,4 +74,33 @@ public class FoxController : MonoBehaviour
             Debug.Log("jumping");
         }
     }
+
+    private void MiniJump()
+    {
+        rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpForce/2);
+    }
+
+    public void Die()
+    {
+        active = false;
+        _collider.enabled = false;
+        MiniJump();
+        Debug.Log("death");
+        StartCoroutine(RespawnPlayer());
+    }
+
+    private IEnumerator RespawnPlayer()
+    {
+        yield return new WaitForSeconds(1f);
+        transform.position = startPosition;
+        active = true;
+        _collider.enabled = true;
+        MiniJump();
+    }
+
+    public void SetStartingPosition()
+    {
+        startPosition = transform.position;
+    }
 }
+
